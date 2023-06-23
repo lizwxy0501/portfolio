7794a9c5fbd8340ab5aec8d5974f4f537011e321
lizwxy0501 <lizwxy0501@icloud.com>
2023-06-23 23:27:15 +0100

    add navbar

--- frontend_react/src/components/Navbar/Narbar.jsx
+++ frontend_react/src/components/Navbar/Narbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Narbar.scss'
+import { images } from '../../assets'
 
 const Narbar = () => {
   return (
-    <div>Narbar</div>
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+            <img src={images.logo} alt='logo'></img>
+        </div>
+        <ul className='app__navbar-links'>
+            {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+                <li className='app__flex p-text' key={`link-${item}`}>
+                    <div>
+                    </div>
+                        <a href={`#{item}`}>
+                            {item}
+                        </a>
+                </li>
+
+            )
+
+            )}
+        </ul>
+    </nav>
   )
 }
 
