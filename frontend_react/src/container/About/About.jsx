eb603e1b9916ea8d42586cdd69ce040d469f095a
lizwxy0501 <lizwxy0501@icloud.com>
2023-06-24 23:17:56 +0100

    add sanity data

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {motion} from 'framer-motion'
 import './About.scss'
 import { images } from '../../assets'
+import { urlFor, client } from '../../client'
 
-
-const abouts = [
-  {title: "Web Design", description: "Figma   UML",imageUrl: images.about01},
-  {title: "Frontend Frameworks", description: "React   Angular   Vue",imageUrl: images.about02},
-  {title: "Programing Languages", description: "Java   Python   Javescript",imageUrl: images.about03},
-  {title: "Database", description: "MySQL   MongoDB   PostgreSQL",imageUrl: images.about04},
-]
+// const abouts = [
+//   {title: "Web Design", description: "Figma UML",imageUrl: images.about01},
+//   {title: "Frontend Frameworks", description: "React Angular Vue",imageUrl: images.about02},
+//   {title: "Programming Languages", description: "Java Python Javascript",imageUrl: images.about03},
+//   {title: "Database", description: "MySQL MongoDB PostgreSQL",imageUrl: images.about04},
+// ]
 
 const About = () => {
 
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+  const query = '*[_type == "abouts"]';  
+  
+  client.fetch(query).then((data) => {
+    // console.log(data)
+    setAbouts(data);
+  });
+
+  }, []);
+  
   return (
     <>
       <h2 className='head-text'>
@@ -28,7 +40,7 @@ const About = () => {
               className='app__profile-item'
               key={about.title + index}
               >
-                <img src={about.imageUrl} alt={about.title}></img>
+                <img src={urlFor(about.imgUrl)} alt={about.title}></img>
                 <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
                 <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
 
