56bdc84be63e005591238ffedd2b1e1c308bbe5b
lizwxy0501 <lizwxy0501@icloud.com>
2023-06-24 11:26:32 +0100

    update navbar scss

--- frontend_react/src/components/Navbar/Narbar.jsx
+++ frontend_react/src/components/Navbar/Narbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Narbar.scss'
 import { images } from '../../assets'
+import {HiMenuAlt4, HiXCircle} from 'react-icons/hi'
+import {motion} from 'framer-motion'
+
+
 
 const Narbar = () => {
+    const [toggle, setToggle] = useState(false)
+
+
   return (
     <nav className='app__navbar'>
         <div className='app__navbar-logo'>
@@ -11,17 +18,33 @@ const Narbar = () => {
         <ul className='app__navbar-links'>
             {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
                 <li className='app__flex p-text' key={`link-${item}`}>
-                    <div>
-                    </div>
+                    <div />
                         <a href={`#{item}`}>
                             {item}
                         </a>
-                </li>
-
-            )
-
-            )}
+                </li>))}
         </ul>
+
+        <div className='app__navbar-menu'>
+                <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+                {toggle && (
+                    <motion.div
+                    whileInView={{x:[300, 0]}}
+                    transition={ {duration: 0.85, ease: 'easeOut'} }>
+
+                        <HiXCircle onClick={() => setToggle(false)}/>
+                        <ul>
+                            {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+                                <li key={item}>
+                                    <a href={`#{item}`} onClick={() => setToggle(false)}>
+                                        {item}
+                                    </a>
+                            </li>))}
+                        </ul>
+                    </motion.div>
+                )}
+        </div>
     </nav>
   )
 }
